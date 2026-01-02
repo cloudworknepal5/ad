2d423312049ad9b0f2357d119a7d0b01bcb01e03
cloudworknepal5 <cloudworknepal5@gmail.com>
2026-01-02 16:52:34 +0545

    Update popup.js

--- client/janawaj/popup.js
+++ client/janawaj/popup.js
@@ -42,7 +42,7 @@
                 <div class="popup-body">
                     <h2>Special Offer!</h2>
                     <p>contact</p>
-                    <a href="https://wa.me/9779811196496" class="popup-button">Click Here</a>
+                    <a href="https://wa.me/9779744229292" class="popup-button">Click Here</a>
                 </div>
             </div>
         </div>
@@ -63,6 +63,7 @@
     window.onclick = (e) => { if (e.target == overlay) overlay.classList.remove('popup-visible'); };
 
 })();
+
 
 
 
