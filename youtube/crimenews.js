944e93ad533f35b3a428c6d7a81fc76ded76309a
cloudworknepal5 <cloudworknepal5@gmail.com>
2026-02-22 14:09:04 +0545

    Update crimenews.js

--- youtube/crimenews.js
+++ youtube/crimenews.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
     apiKey: 'AIzaSyAh5DKuOvbRcLEF3IFdq_XjeFGseKy5LWk',
-    channelId: 'UCnaM-zAbh_-I4Bsd9Yqyjvg',
+    channelId: 'UC09hwXZEtHcScFWebDsj7cA',
     results: 6,
     containerId: 'video-container',
     buttonId: 'btn-load-more'
