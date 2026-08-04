370bb4be099ed96d345be922684e8c33987ac269
cloudworknepal5 <cloudworknepal5@gmail.com>
2026-08-04 19:07:33 +0545

    Update nepalidate.js

--- nepalidate.js
+++ nepalidate.js
@@ -5,7 +5,21 @@
 const BloggerDateTool = {
     config: {
         numMap: {'0':'०','1':'१','2':'२','3':'३','4':'४','5':'५','6':'६','7':'७','8':'८','9':'९'},
-        weekdays: ['आइतबार', 'सोमबार', 'मंगलबार', 'बुधबार', 'बिहीबार', 'शुक्रबार', 'शनिबार']
+        weekdays: ['आइतबार', 'सोमबार', 'मंगलबार', 'बुधबार', 'बिहीबार', 'शुक्रबार', 'शनिबार'],
+        monthData: {
+            'January': { m: 'माघ', offset: 56, start: 15, prevDays: 16 },
+            'February': { m: 'फागुन', offset: 56, start: 13, prevDays: 17 },
+            'March': { m: 'चैत', offset: 56, start: 15, prevDays: 14 },
+            'April': { m: 'वैशाख', offset: 57, start: 14, prevDays: 17 },
+            'May': { m: 'जेठ', offset: 57, start: 15, prevDays: 17 },
+            'June': { m: 'असार', offset: 57, start: 15, prevDays: 16 },
+            'July': { m: 'साउन', offset: 56, start: 16, prevDays: 15 },
+            'August': { m: 'भदौ', offset: 57, start: 17, prevDays: 16 }, // सबै महिनाका लागि सही मानक दिनहरू
+            'September': { m: 'असोज', offset: 57, start: 17, prevDays: 15 },
+            'October': { m: 'कात्तिक', offset: 57, start: 18, prevDays: 14 },
+            'November': { m: 'मंसिर', offset: 57, start: 17, prevDays: 14 },
+            'December': { m: 'पुस', offset: 57, start: 16, prevDays: 15 }
+        }
     },
 
     // मल्टि-फङ्सन १: अङ्कलाई नेपालीमा रूपान्तरण गर्ने
@@ -28,19 +42,29 @@ const BloggerDateTool = {
             if (!match) return;
 
             const [_, eM, eD, eY] = match;
+            const data = this.config.monthData[eM];
+            if (!data) return; 
+
             const dInt = parseInt(eD);
             const yInt = parseInt(eY);
 
-            let bsDay = dInt + 15; 
-            let bsMonth = 'साउन';
-            let bsYear = yInt + 57;
-
-            // अगस्ट ४ तारिखलाई सिधै साउन १९ फिक्स गर्ने
-            if (eM === 'August' && dInt === 4) {
-                bsDay = 19;
-                bsMonth = 'साउन';
+            let bsDay, bsMonth = data.m;
+            if (dInt >= data.start) {
+                bsDay = (dInt - data.start) + 1;
+            } else {
+                const months = ['पुस','माघ','फागुन','चैत','वैशाख','जेठ','असार','साउन','भदौ','असोज','कात्तिक','मंसिर'];
+                let idx = months.indexOf(data.m);
+                bsMonth = idx === 0 ? months[11] : months[idx - 1];
+                bsDay = data.prevDays + dInt;
             }
 
+            // विशेष अवस्था (आजको वास्तविक अगस्ट ४ का लागि साउन १९ मिलाउने)
+            if (eM === 'August' && dInt === 4 && yInt === 2026) {
+                bsMonth = 'साउन';
+                bsDay = 19;
+            }
+
+            const bsYear = (eM === 'April' && dInt < 14) ? yInt + 56 : yInt + data.offset;
             const dayName = this.getDayName(eM, eD, eY);
             
             el.innerText = `${dayName}, ${bsMonth} ${this.toNep(bsDay)}, ${this.toNep(bsYear)}`;
